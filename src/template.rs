3960bd51d4be093861f476c7e965652e2d9e79e0
Anton Österberg <anton.osterberg@protonmail.com>
2022-08-29 23:45:58 +0200

    Do not escape any characters
    Do not escape any characters when doing variable substitution for
    template.

--- src/template.rs
+++ src/template.rs
@@ -1,4 +1,4 @@
-use handlebars::Handlebars;
+use handlebars::{no_escape, Handlebars};
 use std::collections::HashMap;
 
 use crate::prop::Property;
@@ -6,6 +6,7 @@ use crate::prop::Property;
 pub fn substitution(input: String, vars: Vec<Property>) -> Result<String, SubstitutionError> {
     let vars: HashMap<String, String> = merge(vars);
     let mut reg = Handlebars::new();
+    reg.register_escape_fn(no_escape);
     reg.set_strict_mode(true);
     reg.register_template_string("template", input).unwrap();
     match reg.render("template", &vars) {
